01932f7033c99f6bcfd3525a52672cbdab7af1a0
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2026-05-11 21:15:18 +0330

    add custom fonts

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-const title: HTMLHeadElement = document.querySelector("#title");
-
-title.addEventListener("click", () => {
-  const text: string = prompt("How you doing?!");
-  title.textContent = text;
-  console.log("Text content updated!");
-});
