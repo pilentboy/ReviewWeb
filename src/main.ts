c30e7322ea28aa91bc46d074f6740b41d4a2382b
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2026-05-13 20:51:43 +0330

    add mute/unmute btn to intro video

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 const introVideo: HTMLVideoElement = document.querySelector("#intro-video");
 
-introVideo.addEventListener("click", () => {
+const introSoundControlBTN: HTMLButtonElement =
+  document.querySelector("#sound-control-BTN");
+
+introSoundControlBTN.addEventListener("click", () => {
+  console.log("jj");
   if (introVideo.muted) {
     introVideo.muted = false;
+    introSoundControlBTN.innerHTML = `<img class='w-5 h-5 ' src='/icon/sound-mute.png' alt='باز کردن صدا' />`;
   } else {
     introVideo.muted = true;
+    introSoundControlBTN.textContent = "با صدا ببینید";
   }
 });
